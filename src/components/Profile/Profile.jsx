73a7d6e02319e63d2d9235e0db389e6543faa62a
IrynaKyslytsia <i.kislica89@gmail.com>
2023-04-27 18:24:21 +0300

    Change PropTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -36,10 +36,10 @@ Profile.propTypes = {
     tag: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    stats: {
-        followers: PropTypes.string.number,
-        views: PropTypes.string.number,
-        likes: PropTypes.string.number,
-    }
+    stats: PropTypes.exact({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+  }),
 }
 
