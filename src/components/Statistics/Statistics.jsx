6f51d8900fdc65a51c52c26371ebfafb66635e30
IrynaKyslytsia <i.kislica89@gmail.com>
2023-04-27 21:07:16 +0300

    Refactored Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,24 @@
-// import PropTypes from 'prop-types';
-import data from '../../../src/data.json';
-import { Title } from 'components/Title/Title';
-import { StatList } from 'components/StatList/StatList';
+import PropTypes from 'prop-types';
+import data from 'data.json';
 import css from './Statistics.module.css';
 
 export const Statistics = (title, stats) => {
-    return <section className={css.statistic}>
-    <Title
-    title = ""
-     />
-    <StatList
-    stats = {data} />
-    </section>
+    return <section className={css.statistics}>
+    <h2 className={css.title}>{title.length > 0 && title}</h2>
+  
+    <ul className={css.stat_list}>
+    {data.map(({ id, label, percentage }) => (
+        <li className={css.item} key={id}>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+        </li>))}    
+    </ul>
+  </section>
 };
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.array,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
+}
