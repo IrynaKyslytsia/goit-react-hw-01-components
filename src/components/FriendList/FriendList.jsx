eabd3c147b9b6eacb113d8b82e6e56b7490f3a92
IrynaKyslytsia <i.kislica89@gmail.com>
2023-04-29 00:06:06 +0300

    Fixed PropTypes

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from 'components/FriendListItem/FriendListItem';
-import css from './FriendList.module.css'
-
-// import friends from 'friends.json';
+import css from './FriendList.module.css';
 
 export const FriendList = ({ friends }) => {
     return (<ul className={css.friend_list}>
        {friends.map(({ id, avatar, name, isOnline }) => 
          <FriendListItem
-         id={id}
+         key={id}
          avatar={avatar}
          name={name}
          isOnline={isOnline} />
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -12,7 +12,6 @@ export const FriendListItem = ({ id, avatar, name, isOnline }) => {
 };
 
 FriendListItem.propTypes = {
-    id: PropTypes.number.isRequired,
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
