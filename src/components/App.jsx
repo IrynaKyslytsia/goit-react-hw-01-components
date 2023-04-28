05487341514b4aa6c7034ce30d145a1360c92dad
IrynaKyslytsia <i.kislica89@gmail.com>
2023-04-29 00:33:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import user from 'user.json';
 import data from 'data.json';
 import friends from 'friends.json';
+import transactions from '../../src/transactions.json';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
 export const App = () => {
     return <div>
@@ -23,5 +25,10 @@ export const App = () => {
     <FriendList
     friends={friends} 
     />
+
+    <TransactionHistory 
+    items={transactions} 
+    />
+
   </div>
 };
